abd117b1190a5459b76000238300e934147a13ae
iRGBit <birgit@moddr.net>
2012-10-24 22:41:58 +0200

    I work and run with Gmaps

--- sound.crowd/gen/net/noisetube/core/android/BuildInfo.java
+++ sound.crowd/gen/net/noisetube/core/android/BuildInfo.java
@@ -3,5 +3,5 @@ package net.noisetube.core.android;
 			
 public class BuildInfo
 {
-	public static final String timeStamp = "2012-10-24 22:11:57 CEST";
+	public static final String timeStamp = "2012-10-24 22:37:31 CEST";
 }
