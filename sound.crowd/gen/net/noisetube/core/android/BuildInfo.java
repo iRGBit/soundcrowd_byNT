9658ac59c8a20f737041f402e16870b31da5b5ef
iRGBit <birgit@moddr.net>
2012-10-24 22:21:10 +0200

    new

--- sound.crowd/gen/net/noisetube/core/android/BuildInfo.java
+++ sound.crowd/gen/net/noisetube/core/android/BuildInfo.java
@@ -3,5 +3,5 @@ package net.noisetube.core.android;
 			
 public class BuildInfo
 {
-	public static final String timeStamp = "2012-10-24 18:57:24 CEST";
+	public static final String timeStamp = "2012-10-24 22:11:57 CEST";
 }
